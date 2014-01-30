f8be4afa0fdbe82aaca81e1925563d66c7660c67
darkf <lw9k123@gmail.com>
2014-01-29 22:04:03 -0800

    parse numbers

--- Parser.hs
+++ Parser.hs
@@ -8,10 +8,24 @@ data Token = Ident String
 		   | RParen
 		   deriving (Show, Eq)
 
+isIdentifierChar :: Char -> Bool
+isIdentifierChar = (`elem` ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "!?~@#$%^&*-+{}.,/")
+
+splitIdentifier :: String -> (String, String)
+splitIdentifier = break (not . isIdentifierChar)
+
+tryParseNum :: String -> Maybe Double
+tryParseNum str =
+	case reads str of
+		[(x, "")] -> Just x
+		_ -> Nothing
+
 parse :: String -> [Token]
 parse "" = []
 parse (c:str)
 	| c == ' ' || c == '\t' = parse str -- ignore spaces/tabs
 	| otherwise =
-		let (token,rest) = break isSpace (c:str) in
-		Ident token : parse rest
+		let (token,rest) = splitIdentifier (c:str) in
+		case tryParseNum token of
+			Just num -> Number num : parse rest
+			Nothing ->  Ident token : parse rest
--- ParserTests.hs
+++ ParserTests.hs
@@ -4,6 +4,7 @@ import Parser
 
 -- Tokenizer tests
 
+-- Identifiers
 identifiers_test = TestCase $ do
 	parse "x" @?= [Ident "x"]
 	parse "xyz" @?= [Ident "xyz"]
@@ -14,9 +15,19 @@ identifier_stream = TestCase $ do
 	parse "x y z" @?= [Ident "x", Ident "y", Ident "z"]
 	parse "xyz abc? def" @?= [Ident "xyz", Ident "abc?", Ident "def"]
 
+-- Numbers
+numbers_test = TestCase $ do
+	parse "123" @?= [Number 123.0]
+	parse "123.0" @?= [Number 123.0]
+	parse "123.5" @?= [Number 123.5]
+	parse "123.75" @?= [Number 123.75]
+	parse "1.5 2.3 4" @?= [Number 1.5, Number 2.3, Number 4]
+
 -- Test group
 
 parserTests = TestList [
 	  TestLabel "identifiers" identifiers_test,
-	  TestLabel "identifier_stream" identifier_stream
+	  TestLabel "identifier_stream" identifier_stream,
+
+	  TestLabel "numbers_test" numbers_test
 	]
