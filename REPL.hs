f670ea0e5b3a5134ed19fa90f05361a8c3d32bec
darkf <lw9k123@gmail.com>
2014-02-02 21:26:16 -0800

    REPL: add load file command

--- REPL.hs
+++ REPL.hs
@@ -1,7 +1,7 @@
 module REPL where
 import Parser (parseStringWith)
 import Interpreter (StateI, currentArities, interpret', exec)
-import System.IO (stdout, hFlush)
+import System.IO (stdout, hFlush, readFile)
 import Control.Monad.IO.Class (liftIO)
 
 repl :: StateI ()
@@ -9,8 +9,16 @@ repl = do
 	liftIO $ putStr "> " >> hFlush stdout
 	line <- liftIO getLine
 	arities <- currentArities
-	val <- interpret' $ parseStringWith arities line
-	liftIO $ print val
-	repl
+	case line of
+		":q" -> return ()
+		':':'l':' ':filename -> do
+			-- load filename
+			file <- liftIO $ readFile filename
+			interpret' $ parseStringWith arities file
+			repl
+		_ -> do
+			val <- interpret' $ parseStringWith arities line
+			liftIO $ print val
+			repl
 
 main = exec repl >> return ()
